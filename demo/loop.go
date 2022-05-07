0229454bacc7132ef9a759efa58e3a997a9332b3
shulu <961085397@qq.com>
2022-05-07 14:59:28 +0800

    loop & array
    loop & array

--- demo/loop.go
+++ demo/loop.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -46,4 +47,45 @@ func main() {
 	} else {
 		fmt.Println(num, "has multiple digits")
 	}
+
+	//switch
+	it := 2
+	fmt.Println("Write", it, " as ")
+	switch it {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Its's weekend")
+	default:
+		fmt.Println("It's weekday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
